e19cbe6a15e27b5b60bbf0f641f22cd486f9e3ce
ankurraj <ankurrajdutta@gmail.com>
2022-04-28 23:30:44 +0530

    Task3

---
+++ dom.js
@@ -0,0 +1,10 @@
+
+// console.dir(document);
+// console.log(document.URL);
+// console.log(document.all[22]);
+const header=document.getElementById('main-header');
+header.style.borderBottom="solid 3px #000";
+
+const addItems=document.getElementsByTagName('h2');
+addItems[0].style.fontWeight="bold";
+addItems[0].style.color="green";
