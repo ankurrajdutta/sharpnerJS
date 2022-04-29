79c8075e91f4ef608b110279c0287fdc3f8c7bda
ankurraj <ankurrajdutta@gmail.com>
2022-04-29 22:44:35 +0530

    task4

--- dom.js
+++ dom.js
@@ -2,9 +2,20 @@
 // console.dir(document);
 // console.log(document.URL);
 // console.log(document.all[22]);
-const header=document.getElementById('main-header');
-header.style.borderBottom="solid 3px #000";
+// const header=document.getElementById('main-header');
+// header.style.borderBottom="solid 3px #000";
 
-const addItems=document.getElementsByTagName('h2');
-addItems[0].style.fontWeight="bold";
-addItems[0].style.color="green";
+// const addItems=document.getElementsByTagName('h2');
+// addItems[0].style.fontWeight="bold";
+// addItems[0].style.color="green";
+
+const items=document.getElementsByClassName('list-group-item');
+console.log(items);
+
+items[0].textContent='Hello';
+items[0].style.fontWeight='bold';
+
+for(var i=0;i<items.length;i++){
+    items[i].style.background='#f4f4f4';
+}
+
