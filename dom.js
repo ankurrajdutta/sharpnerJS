25b9ffb2046de675091dbac1e7c25c733a80872b
ankurraj <ankurrajdutta@gmail.com>
2022-05-01 12:30:05 +0530

    task7

--- dom.js
+++ dom.js
@@ -28,14 +28,14 @@
 //     items1[i].style.background='red';
 // }
 
-var container=document.querySelector('.container');
-container.style.borderBottom='solid 4px #ccc';
+// var container=document.querySelector('.container');
+// container.style.borderBottom='solid 4px #ccc';
 
-var title=document.querySelector('input');
-title.value='Hello';
+// var title=document.querySelector('input');
+// title.value='Hello';
 
-var submit=document.querySelector('input[type="submit"]');
-submit.value='send';
+// var submit=document.querySelector('input[type="submit"]');
+// submit.value='send';
 
 // var item=document.querySelector('.list-group-item');
 // item.style.backgroundColor='red';
@@ -46,14 +46,49 @@ submit.value='send';
 // var secondItem=document.querySelector('.list-group-item:nth-child(2)');
 // secondItem.style.backgroundColor='orange';
 
-var odd=document.querySelectorAll('li:nth-child(odd)');
-var even=document.querySelectorAll('li:nth-child(even)');
-console.log(odd);
-console.log(even);
-for(var i=0;i<odd.length;i++){
-    odd[i].style.backgroundColor='#f4f4f4';
-    even[i].style.backgroundColor='#ccc'
-}
+// var odd=document.querySelectorAll('li:nth-child(odd)');
+// var even=document.querySelectorAll('li:nth-child(even)');
+// console.log(odd);
+// console.log(even);
+// for(var i=0;i<odd.length;i++){
+//     odd[i].style.backgroundColor='#f4f4f4';
+//     even[i].style.backgroundColor='#ccc'
+// }
+
+// var list=document.querySelector('#items');
+// list.parentElement.style.backgroundColor='#f4f4f4';
+// list.parentNode.style.backgroundColor='#ccc';
+// console.log(list.childNodes);
+// console.log(list.children);
+// list.children[0].textContent="Hello 1";
+// console.log(list.firstChild);
+// console.log(list.firstElementChild);
+// console.log(list.lastChild);
+// console.log(list.lastElementChild);
+// list.lastElementChild.textContent="Hello 4";
+
+var newDiv=document.createElement('div');
+newDiv.className="hello";
+newDiv.id="hello1";
+newDiv.setAttribute('title','Hello Div');
+var newText=document.createTextNode('Hello WOrld');
+newDiv.appendChild(newText);
+var container=document.querySelector('header .container');
+var h1=document.querySelector('header h1');
+container.insertBefore(newDiv,h1);
+
+var items=document.querySelector('#items');
+var newList=document.createElement('li');
+var newText1=document.createTextNode('Hello WOrld');
+newList.className="list-group-item";
+newList.appendChild(newText1);
+var li=document.querySelector('li');
+items.insertBefore(newList,li);
+
+
+
+
+
 
 
 
