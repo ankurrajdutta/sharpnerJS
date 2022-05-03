1caf3daccc4fed1856f38291277ba7be4e2f92c9
ankurraj <ankurrajdutta@gmail.com>
2022-05-03 21:43:24 +0530

    task12

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
-// var form=document.getElementById('my-form');
-// form.addEventListener('submit',addDetails);
+var form=document.getElementById('my-form');
+var users=document.getElementById('users');
+form.addEventListener('submit',addDetails);
+let count=0;
+function addDetails(e){
+    e.preventDefault();
+    var name=document.getElementById('name').value;
+    var email=document.getElementById('email').value;
+    var myObj={
+        kay_name:name,
+        kay_email:email
+    }
+   
+    localStorage.setItem('myObj'+name,JSON.stringify(myObj));
+    count++;
+    var myObj_deserialized=JSON.parse(document.getEle)
 
-// function addDetails(e){
-//     e.preventDefault();
-//     var name=document.getElementById('name').value;
-//     var email=document.getElementById('email').value;
-//     localStorage.setItem('name',name);
-//     localStorage.setItem('email',email);
-// }
+    var li=document.createElement('li');
+    li.appendChild(document.createTextNode())
+}
 
-const myObj={
-    name:"ankur",
-    age: 24
-};
+// const myObj={
+//     name:"ankur",
+//     age: 24
+// };
 
-let myObj_serialized=JSON.stringify(myObj);
-localStorage.setItem('myObj',myObj_serialized);
-console.log(localStorage);
+// let myObj_serialized=JSON.stringify(myObj);
+// localStorage.setItem('myObj',myObj_serialized);
+// console.log(localStorage);
 
-let myObj_deserialized=JSON.parse(localStorage.getItem("myObj"));
-console.log(myObj_deserialized);
+// let myObj_deserialized=JSON.parse(localStorage.getItem("myObj"));
+// console.log(myObj_deserialized);
